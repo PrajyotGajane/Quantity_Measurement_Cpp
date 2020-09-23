95934710f3b653b64dd62b58c24ee5f34f04b44b
PrajyotGajane <prajyotgajane@gmail.com>
2020-09-24 03:24:06 +0530

    Added: CENTIMETER to UnitType enum

--- main/QuantityMeasurement.h
+++ main/QuantityMeasurement.h
@@ -8,5 +8,4 @@ public:
     double getConvertedValue(Unit, double);
     bool operator==(QuantityMeasurement *);
     bool operator==(QuantityMeasurement);
-    double addTwoUnits(double,double);
 };
