58c301fb3e4c6b312a982f3e326e254c43201568
PrajyotGajane <prajyotgajane@gmail.com>
2020-09-24 00:28:20 +0530

    Added: Test case to check 1 feet and 12 inch are same

--- test/QuantityMeasurementTest.cpp
+++ test/QuantityMeasurementTest.cpp
@@ -4,15 +4,17 @@
 // Set up to use Quantity Measurement reference in multiple test cases
 struct QuantityMeasurementTest : testing::Test
 {
-	QuantityMeasurement quantity_measurement;
-    QuantityMeasurement* quantity_measurement_ref;
-    QuantityMeasurementTest() {
-		quantity_measurement_ref = new QuantityMeasurement();
-	}
+    QuantityMeasurement quantity_measurement;
+    QuantityMeasurement *quantity_measurement_ref;
+    QuantityMeasurementTest()
+    {
+        quantity_measurement_ref = new QuantityMeasurement();
+    }
 
-	~QuantityMeasurementTest() {
-		delete quantity_measurement_ref;
-	} 
+    ~QuantityMeasurementTest()
+    {
+        delete quantity_measurement_ref;
+    }
 };
 
 //UC 1
@@ -25,8 +27,8 @@ TEST_F(QuantityMeasurementTest, _zero_feet_and_zero_feet__should_be_equal)
 
 TEST(QuantityMeasurement_Test, first_object_and_second_ref_should_be_equal)
 {
-    QuantityMeasurement* first_ref = new QuantityMeasurement();
-    QuantityMeasurement* second_ref = first_ref;
+    QuantityMeasurement *first_ref = new QuantityMeasurement();
+    QuantityMeasurement *second_ref = first_ref;
     ASSERT_EQ(first_ref, second_ref);
 }
 
@@ -39,9 +41,16 @@ TEST(QuantityMeasurement_Test, first_object_and_second_object_type_should_be_equ
 
 TEST(QuantityMeasurement_Test, first_object_and_second_object_value_should_be_equal)
 {
-    Unit first_feet(1,UnitType::LENGTH);
-    Unit second_feet(1,UnitType::LENGTH);
+    Unit first_feet(1, UnitType::LENGTH);
+    Unit second_feet(1, UnitType::LENGTH);
     ASSERT_TRUE(first_feet == second_feet);
+}
+
+TEST_F(QuantityMeasurementTest, given_one_feet_and_twelve_inch_should_return_equal)
+{
+    double result1 = quantity_measurement.returnCovertedValue(Unit::FEET, 1);
+    double result2 = quantity_measurement.returnCovertedValue(Unit::INCH, 12);
+    ASSERT_EQ(result1, result2);
 }
 
 int main(int argc, char **argv)
