a6e9ac336ab6cf3fcfed09e8c56956ea58b24eb1
PrajyotGajane <prajyotgajane@gmail.com>
2020-09-23 23:14:29 +0530

    Added: Test case to check reference of objects

--- test/QuantityMeasurementTest.cpp
+++ test/QuantityMeasurementTest.cpp
@@ -5,13 +5,29 @@
 struct QuantityMeasurementTest : testing::Test
 {
 	QuantityMeasurement quantity_measurement;
+    QuantityMeasurement* quantity_measurement_ref;
+    QuantityMeasurementTest() {
+		quantity_measurement_ref = new QuantityMeasurement();
+	}
+
+	~QuantityMeasurementTest() {
+		delete quantity_measurement_ref;
+	} 
 };
 
+//UC 1
 TEST_F(QuantityMeasurementTest, _zero_feet_and_zero_feet__should_be_equal)
 {
     double first_value = quantity_measurement.returnCovertedValue(Unit::FEET, 0);
     double second_value = quantity_measurement.returnCovertedValue(Unit::FEET, 0);
     ASSERT_EQ(first_value, second_value);
+}
+
+TEST(QuantityMeasurement_Test, first_object_and_second_ref_should_be_equal)
+{
+    QuantityMeasurement* first_ref = new QuantityMeasurement();
+    QuantityMeasurement* second_ref = first_ref;
+    ASSERT_EQ(first_ref, second_ref);
 }
 
 int main(int argc, char **argv)
